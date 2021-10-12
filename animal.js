2c961616537b03e9a84c8379f0bf151a7d9a76cb
Aalmani1 <mem.mem6789@gmail.com>
2021-10-13 01:47:57 +0300

    v2

--- animal.js
+++ animal.js
@@ -6,45 +6,66 @@ class Animal {
     this.sound = sound;
   }
 
-  eats(name) {
+  eats() {
     console.log(this.name + " eats food");
   }
-  Animalsound(sound) {
+  Animalsound() {
     console.log(this.sound + " sounds is " + this.sound);
   }
 }
-let animal = new Animal("cats", "mew");
-animal.eats("cats");
-animal.Animalsound("mew");
 
 //==================================================
 
 class Cat extends Animal {
+  constructor(name, age, img, sound, owner) {
+    super(name, age, img, sound);
+    this.owner = owner;
+  }
   eats() {
     console.log("cats eats mouse");
   }
   Animalsound() {
-    console.log("cat sounds is meow");
+    console.log(this.name + " sounds is " + this.sound);
   }
 }
 
 //=====================================================
 
 class Dog extends Animal {
+  constructor(name, age, img, sound, owner) {
+    super(name, age, img, sound);
+    this.owner = owner;
+  }
   eats() {
     console.log("Dog eats chicken");
   }
   Animalsound() {
-    console.log("Dogs sounds is woof");
+    console.log(this.name + " sounds is " + this.sound);
   }
 }
 
 //=======================================================
 
 class Fish extends Animal {
+  constructor(name, age, img, owner) {
+    super(name, age, img);
+    this.color = color;
+  }
   eats() {
     console.log("fish eats flakes");
   }
 }
 
-//===================
+//=======================================================
+
+let cat = new Cat("cats", 1, "cat.jpeg", "mew", "amirah");
+cat.eats();
+cat.Animalsound();
+
+let dog = new Dog("Dogs", 1, "dog.jpeg", "woof", "amirah");
+dog.eats();
+dog.Animalsound();
+
+let fish = new Fish("Fish", 1, "fish.jpeg", "amirah");
+fish.eats();
+fish.Animalsound();
