2ee3a4745c254cc39aef243c59605cfdc096e3e0
yazeed aleid <yazzed.aleid@gmail.com>
2021-10-12 19:25:22 +0300

    W02D03-OOP-HW

--- ex1.js
+++ ex1.js
@@ -1,10 +1,10 @@
 let movie = {
-    name:'frozen',
+    name:'Frozen',
     duration:'2h',
-    stars:["Elsa",'Ana','olaf']
+    stars:["Elsa",'Ana','Olaf']
 
 }
 console.log(movie.name+" lasts for "+movie.duration+" Stars "+movie.stars)
-let div = document.createElement('div');
-div = `${movie.name} lasts for ${movie.duration} Stars ${movie.stars}`
+let div = document.createElement('h1');
+div.innerText= `${movie.name} lasts for ${movie.duration} Stars ${movie.stars}`
 document.body.append(div)
