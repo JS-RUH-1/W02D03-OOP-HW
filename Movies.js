8acf7ff7223f09b2586d57067edf2929e89bf526
Emad Aldomari <ea@ea.sa>
2021-10-12 14:46:44 +0300

    done hw

---
+++ Movies.js
@@ -0,0 +1,8 @@
+let movie = {
+    title: "Puff the Magic Dragon",
+    duration: 30,
+    stars: ["Puff", "Jackie", "Living Sneezes"],
+};
+  
+console.log(movie.title, "lasts for", movie.duration, "minutes.", "It stars", movie.stars.join(", "));
+
